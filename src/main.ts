9868892445d757febd3f8b88eb8f148944478245
Sam Zanca <sam@zanca.dev>
2023-02-05 14:25:48 -0500

    Basic canvas

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,49 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function resize(canvas) {
+  canvas.height = document.body.clientHeight
+  canvas.width = document.body.clientWidth
+}
+
+function clear(canvas, ctx, { bg = '#282a36' } = {}) {
+  ctx.fillStyle = bg
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawSquare(ctx, { x = 0, y = 0, size = 100, color = 'blue' } = {}) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, size, size);
+}
+
+function main() {
+  const canvas = document.getElementById('canvas')! as HTMLCanvasElement
+  const context = canvas.getContext("2d")
+
+  let x = 0, y = 0;
+
+  // Renderer
+  const rerender = () => {
+    resize(canvas)
+    clear(canvas, context)
+
+    drawSquare(context, {
+      x, y, size: 200, color: 'blue'
+    })
+  }
+
+
+  // Events
+  canvas.addEventListener('click', e => {
+    console.log(e.clientX, e.clientY)
+    x = e.clientX
+    y = e.clientY
+
+    rerender()
+  })
+
+  // Initial Paint
+  rerender()
+}
+
+main()
