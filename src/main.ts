6b5f26df8ae041b9bfd88bf98b27053d4238bd4f
Sam Zanca <sam@zanca.dev>
2023-02-05 14:34:40 -0500

    Canvas class

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,51 @@
 import './style.css'
 
-
+/* Sets canvas size to be equal to body */
 function resize(canvas) {
   canvas.height = document.body.clientHeight
   canvas.width = document.body.clientWidth
 }
 
+/* Clear the canvas */
 function clear(canvas, ctx, { bg = '#282a36' } = {}) {
   ctx.fillStyle = bg
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+/* Draw a square to the canvas */
 function drawSquare(ctx, { x = 0, y = 0, size = 100, color = 'blue' } = {}) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, size, size);
 }
 
-function main() {
-  const canvas = document.getElementById('canvas')! as HTMLCanvasElement
-  const context = canvas.getContext("2d")
+/* Handles drawing the canvas and abstracts the coordinate system */
+class InfiniteCanvas {
+  private canvas;
+  private context;
+  x = 0;
+  y = 0;
 
-  let x = 0, y = 0;
-
-  // Renderer
-  const rerender = () => {
-    resize(canvas)
-    clear(canvas, context)
-
-    drawSquare(context, {
-      x, y, size: 200, color: 'blue'
-    })
+  debug() {
+    console.log(`x: ${this.x}, y: ${this.y}`)
   }
 
+  render() {
+    resize(this.canvas)
+    clear(this.canvas, this.context)
 
-  // Events
-  canvas.addEventListener('click', e => {
-    console.log(e.clientX, e.clientY)
-    x = e.clientX
-    y = e.clientY
+    drawSquare(this.context, {
+      x: this.x, y: this.y, size: 200, color: 'blue'
+    })
 
-    rerender()
-  })
+    this.debug()
+  }
+  constructor(canvas) {
+    this.canvas = canvas
+    this.context = canvas.getContext("2d")
 
-  // Initial Paint
-  rerender()
+    this.render()
+  }
 }
 
-main()
+const canvas = document.getElementById('canvas')
+const instance = new InfiniteCanvas(document.getElementById('canvas'))
